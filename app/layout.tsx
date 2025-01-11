4a6135655e24d91ed01e8b9340c921b95cdd1432
dev <dev@ninjapay.me>
2025-01-11 21:50:03 +0000

    added ui ux

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,7 +1,7 @@
 import type { Metadata } from 'next'
 import localFont from 'next/font/local'
 import './globals.css'
-import { Header } from './components/header'
+import { Header } from './components/Header'
 import 'react-grid-layout/css/styles.css';
 import 'react-resizable/css/styles.css';
 
